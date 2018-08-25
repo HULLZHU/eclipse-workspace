2f4677e80d1efb6f935234f45bcd374f0098a3b8
HULLZHU <42597991+HULLZHU@users.noreply.github.com>
2018-08-25 14:00:46 -0400

    Update BinaryTree.java
    Excited to have successfully finish the binary tree model by myself based on JAVAFX. This GUI prompts user to enter Risk Free Rate, stock price volatility, and some other important parameter, and then giving the user a easy-understanding window to know how the possible final result distribute.

--- Chap14/src/BinaryTree.java
+++ Chap14/src/BinaryTree.java
@@ -1,6 +1,7 @@
 import javafx.scene.paint.*;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.application.Application;
 import javafx.scene.layout.Pane;
@@ -10,7 +11,7 @@ public class BinaryTree extends Pane {
 private Color colorOfNodes =Color.GREEN;
 private Color colorOfLine = Color.RED;
 private Color colorOfText = Color.BLACK;
-private int stepNumber = 10;
+private int stepNumber = 12;
 private double riskfreeRate = 0.06;
 private double volatility = 0.1;
 private double stockPrice = 10;
@@ -20,25 +21,32 @@ private double d = Math.exp(-Math.sqrt(deltaT)*volatility);
 private double a = Math.exp(riskfreeRate*deltaT);
 private double upProb = (a-d)/(u-d);
 private Circle[][] nodes = new Circle[stepNumber][];
-private int testTime = 100;
-
-private double startPointY =stepNumber*40;
+private boolean lineVisible =true;
+private double startPointY =stepNumber*50;
 private double startPointX = 50;
-
+private double [][] prices = new double[stepNumber][];
+private double [] counts = new double[stepNumber];
+private double totalHeight =800;
+private int testNumber;
 
 public BinaryTree(double riskfreeRate,double volatility, double stockPrice,double deltaT,int stepNumber) {
 	this.riskfreeRate = riskfreeRate;
 	this.volatility = volatility;
 	this.stockPrice = stockPrice;
 	this.deltaT = deltaT;
-	this.stepNumber = stepNumber;
+	setStepNumber(stepNumber);
     u = Math.exp(Math.sqrt(deltaT)*volatility);
     d = Math.exp(-Math.sqrt(deltaT)*volatility);
 	a = Math.exp(riskfreeRate*deltaT);
 	upProb = (a-d)/(u-d);
-	nodes = new Circle[stepNumber][];
+	nodes = new Circle[stepNumber+1][];
+	startPointY =stepNumber*50;
+	startPointX = 50;
+	prices = new double[stepNumber+1][];
+	counts = new double[stepNumber+1];
 }
-
+public void setStepNumber(int stepNumber) {
+	this.stepNumber = stepNumber;}
 public double getRiskfreeRate() {
 	return riskfreeRate;
 }
@@ -72,26 +80,86 @@ public void setDeltaT(double deltaT) {
 }
 
 
-
-public void getSimulation() {
+public void getSimulation(int testNumber) {
+	this.testNumber = testNumber;
 	addSummary();
 	drawNodes();
-	drawLines();
-	addDescriptions();
+	if (lineVisible)
+	  drawLines();
+	getPrices();
+	
+	//start test
+	for ( int i = 0 ; i < testNumber ; i ++ ) {
+		startTest();
+	}
+	//add the frequency text
+	addFinalResultDescription();
+	//add statistical bar charts
+	addBars();
+	
+}
+private void addBars() {
+	double barWidth = 20;
+	for (int i = 0; i < counts.length ;i ++ ) {
+		double ratio =counts[i]/this.testNumber;
+		double barHeight = totalHeight*ratio;
+		Rectangle bar = new Rectangle();
+		bar.setHeight(barWidth);
+		bar.setWidth(barHeight);
+		bar.setX(nodes[nodes.length-1][i].getCenterX()+100);
+		bar.setY(nodes[nodes.length-1][i].getCenterY()-10);
+		bar.setFill(Color.LIGHTGREEN);
+		this.getChildren().add(bar);
+	}
+	
+}
+private void addFinalResultDescription() {
+	for ( int i = 0 ; i < counts.length ; i ++ ) {
+		Text text = new Text((int)counts[i]+"");
+		text.setX(nodes[nodes.length-1][i].getCenterX()+30);
+		text.setY(nodes[nodes.length-1][i].getCenterY()+5);
+		this.getChildren().add(text);
+	}
+}
+private void startTest() {
+	int status = 0;
+	for ( int i = 0 ; i < stepNumber  ; i ++ ) {
+		double temp = Math.random();
+		if ( temp >= upProb)
+			status ++;
+		else
+			continue;
+	}
+	counts[status]++;
 }
 
-private void addDescriptions() {
-	
+private void getPrices() {
+	for ( int i = 0; i < prices.length ; i ++ ) {
+		prices[i]  = new double[nodes[i].length];
+		for ( int j = 0; j < prices[i].length; j++) {
+			if ( i == 0 )
+				prices[i][j]=stockPrice;
+			else if ( j < prices[i].length - 1)
+				prices[i][j]= prices[i - 1][j]*(1+volatility);
+			else if ( j == prices[i].length -1)
+				prices[i][j] = prices[i-1][j-1]/(1+volatility);
+			
+			Text priceText = new Text(Math.round(prices[i][j]*100)/(100.0)+"");
+			priceText.setX(nodes[i][j].getCenterX()-15);
+			priceText.setY(nodes[i][j].getCenterY()+15);
+			this.getChildren().add(priceText);
+		}
+	}
 	
 }
 
 private void drawLines() {
-	for ( int i = 0; i < stepNumber; i ++ ) {
+	for ( int i = 0; i < nodes.length; i ++ ) {
 		Line line = new Line();
 		line.setStartX(nodes[i][i].getCenterX());
 		line.setStartY(nodes[i][i].getCenterY());
-		line.setEndX(nodes[stepNumber-1][i].getCenterX());
-		line.setEndY(nodes[stepNumber-1][i].getCenterY());
+		line.setEndX(nodes[nodes.length-1][i].getCenterX());
+		line.setEndY(nodes[nodes.length-1][i].getCenterY());
 		this.getChildren().add(line);
 	}
 	
@@ -99,8 +167,8 @@ private void drawLines() {
 		Line line = new Line();
 		line.setStartX(nodes[i][0].getCenterX());
 		line.setStartY(nodes[i][0].getCenterY());
-		line.setEndX(nodes[stepNumber-1][stepNumber-i-1].getCenterX());
-		line.setEndY(nodes[stepNumber-1][stepNumber-i-1].getCenterY());
+		line.setEndX(nodes[nodes.length-1][nodes.length-i-1].getCenterX());
+		line.setEndY(nodes[nodes.length-1][nodes.length-i-1].getCenterY());
 		this.getChildren().add(line);
 	}
 	
@@ -114,8 +182,11 @@ public void setStartPoint(double x,double y) {
 	this.startPointY =y;
 }
 
+public void setLineVisible(boolean visible) {
+	lineVisible = visible;
+}
+
 private void drawNodes() {
-	
 	for ( int i = 0; i < nodes.length ; i++) {
 		nodes[i] = new Circle[i + 1];
 		nodes[i][0] = new Circle();
@@ -132,18 +203,13 @@ private void drawNodes() {
 			nodes[i][j].setRadius(10);
 			nodes[i][j].setFill(colorOfNodes);
 		}
-	}
-	
+	}	
 }
 
 private void addSummary() {
-	String string = "Risk Free Rate: "+ this.riskfreeRate+ "\nVolatility: "+this.getVolatility()+"\ndeltaT: "+this.deltaT+"\nu:"+this.u
+	String string = "Risk Free Rate: "+ this.riskfreeRate*100+ "\nVolatility: "+this.getVolatility()+"\ndeltaT: "+this.deltaT+"\nu:"+this.u
 			+"\nd:"+this.d+"\nupProb:"+this.upProb;
 	Text text = new Text(this.startPointX-15,this.startPointY+30*stepNumber,string);
 	this.getChildren().add(text);
 }
-
-
-
-
 }
